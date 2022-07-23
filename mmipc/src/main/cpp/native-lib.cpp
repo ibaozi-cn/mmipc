4daa7e20ee79bcc99d9f00eb9df904671a344e8a
校长 <zzy0523@gmail.com>
2022-07-23 18:17:38 +0800

    重构代码，添加单例，函数抽离，合理分工

--- mmipc/src/main/cpp/native-lib.cpp
+++ mmipc/src/main/cpp/native-lib.cpp
@@ -1,38 +1,34 @@
 #include <jni.h>
 #include "MMIPC.h"
 #include "AndroidLog.h"
-
-static MMIPC *mmipc = nullptr;
+#include "Configs.h"
 
 extern "C"
 JNIEXPORT void
 JNICALL
 Java_com_zzy_mmipc_MMIPC_initMMAPJNI(JNIEnv *env, jobject thiz, jstring root_dir) {
     const char *dir = env->GetStringUTFChars(root_dir, nullptr);
-    ALOGD("open file %s", dir);
-    mmipc = new MMIPC();
-    mmipc->reloadMmap(dir);
+    ALOGD("root dir %s", dir);
+    string rootDir(dir);
+    configs::get_instance().g_rootDir = rootDir;
+    mmipc::get_instance().reloadMmap(dir);
 }
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_zzy_mmipc_MMIPC_setDataJNI(JNIEnv *env, jobject thiz, jstring key, jstring value) {
     const char *keyStr = env->GetStringUTFChars(key, nullptr);
     const char *valueStr = env->GetStringUTFChars(value, nullptr);
-    if (mmipc) {
-        mmipc->setData(keyStr, valueStr);
-    }
+    mmipc::get_instance().setData(keyStr, valueStr);
 }
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_zzy_mmipc_MMIPC_getDataJNI(JNIEnv *env, jobject thiz, jstring key, jstring defaultValue) {
     const char *keyStr = env->GetStringUTFChars(key, nullptr);
     const char *defaultValueStr = env->GetStringUTFChars(defaultValue, nullptr);
-    if (mmipc) {
-        std::string dataStr = mmipc->getData(keyStr, defaultValueStr);
-        if (dataStr.empty()) {
-            ALOGD("getData empty by key%s", keyStr);
-            dataStr = defaultValueStr;
-        }
-        return env->NewStringUTF(dataStr.c_str());
+    std::string dataStr = mmipc::get_instance().getData(keyStr, defaultValueStr);
+    if (dataStr.empty()) {
+        ALOGD("getData empty by key%s", keyStr);
+        dataStr = defaultValueStr;
     }
+    return env->NewStringUTF(dataStr.c_str());
 }
